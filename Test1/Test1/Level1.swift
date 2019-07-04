0e995f1e1125a5366e7ffaf7c4ffb94dc2f2d023
fedepini <fpini95@gmail.com>
2019-07-04 17:25:30 +0200

    Added comments to explain the three different AudioKit oscillators

--- Test1/Test1/Level1.swift
+++ Test1/Test1/Level1.swift
@@ -79,17 +79,24 @@ class Level1: UIViewController {
         
         guard gestureRecognizer.view != nil else {return}
         
-        // Update the position for the .began, .changed, and .ended states
+        // Updates the position for the .began, .changed, and .ended states
         
         if gestureRecognizer.state != .cancelled {
             print(initialPoint)
             
             let firstLevelRect = firstLevelShape.getCGRect()
             
-            // Prints OK if the point is inside the shape, NO otherwise
+            // Distinguishes 3 cases based on the finger position:
+            // 1. Inside the line but not in the center
+            // 2. At the center of the line
+            // 3. Outside the line
+            
+            // The finger is inside the line
             
             if (firstLevelRect.contains(initialPoint)) {
                 print("OK: point is inside shape")
+                
+                // 1. Inside the line but not in the center
                 
                 oscillatorMid.stop()
                 oscillator2.stop()
@@ -97,12 +104,16 @@ class Level1: UIViewController {
                 oscillator.amplitude = 1
                 oscillator.start()
                 
+                // Creates a sub-shape which indicates the line center
+                
                 let y = self.view.frame.size.height / 2 - label1.frame.height / 2 - 25 + 37.5
                 let minY = y - 5
                 let maxY = y + 5
                 
                 let middleLineX = label1.frame.maxX + 10..<label2.frame.minX - 10
                 let middleLineY = minY..<maxY
+                
+                // 2. At the center of the line
      
                 if(middleLineX.contains(initialPoint.x) && middleLineY.contains(initialPoint.y)) {
                     print("Inside the middle line")
@@ -113,9 +124,15 @@ class Level1: UIViewController {
                     
                     oscillatorMid.baseFrequency = 500
                     oscillatorMid.start()
-                } else {panner.pan = 0.0}
+                } else {
+                    panner.pan = 0.0
+                    
+                }
                 
             } else {
+                
+                // 3. Outside the line
+                
                 print("NO: point is outside shape")
                 
                 oscillatorMid.stop()
@@ -126,6 +143,8 @@ class Level1: UIViewController {
             }
         }
     }
+    
+    // Normalizes double values for AudioKit panner
     
     func normalize(num: Double) -> Double {
         let min = Double(label1.frame.maxX + 10)
